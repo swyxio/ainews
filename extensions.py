6f80a92a320116d97a678f3b57f988b9aaeedfb9
scottdelap <sdelap@gmail.com>
2024-08-07 14:57:20 -0500

    First pass at adding a verb type to submissions.  Schema shadowing might need a little cleanup. Today it denormalizes a lot in submission, source, topic overlap.  Ok for now.  Record of user title vs final topic title might be useful.

--- extensions.py
+++ extensions.py
@@ -29,18 +29,42 @@ def display_time(timestr):
         created_at_str = 'N/A'
     return created_at_str
 
-def display_url(url, title, timestr, owner):
+def display_url(url, type, title, timestr, owner, state):
     created_at = display_time(timestr)
     from urllib.parse import urlparse
     try:
         parsed_url = urlparse(url)
+        prefix_type = f"{type.capitalize()}: " if type != "" else ""
         if parsed_url.netloc.startswith('www.'):
             parsed_url = parsed_url._replace(netloc=parsed_url.netloc[4:])
-        show = Div(Span(title), 
-                   Span(f"{created_at} by {owner}", cls="text-xs text-gray-400")
+        show = Div(
+                  Span(A2(f"{prefix_type}{title}", href=url, target="_blank")), 
+                  Span(f"({created_at} by {owner} {state})", cls="text-xs text-gray-400"), 
+                  cls="flex flex-col"
               ) if parsed_url.netloc == '' else Div(
-                  Span(A2(title, href=url, target="_blank")), 
-                  Span(f"({parsed_url.netloc}, {created_at} by {owner})", cls="text-xs text-gray-400"), 
+                  Span(A2(f"{prefix_type}{title}", href=url, target="_blank")), 
+                  Span(f"({parsed_url.netloc}, {created_at} by {owner} {state})", cls="text-xs text-gray-400"), 
+                  cls="flex flex-col"
+              )
+    except ValueError:
+        show = Span(title) if url is None else Span(A2(title, href=url), 'NA')
+    return show
+
+def display_submission_url(submission_state, url, type, title, timestr, owner, state):
+    created_at = display_time(timestr)
+    from urllib.parse import urlparse
+    try:
+        parsed_url = urlparse(url)
+        prefix_type = f"{type.capitalize()}: " if type != "" else ""
+        if parsed_url.netloc.startswith('www.'):
+            parsed_url = parsed_url._replace(netloc=parsed_url.netloc[4:])
+        show = Div(
+                  Span(A2(f"Submission State: {submission_state} {prefix_type}{title}", href=url, target="_blank")), 
+                  Span(f"{created_at} by {owner} {state})", cls="text-xs text-gray-400"), 
+                  cls="flex flex-col"
+              ) if parsed_url.netloc == '' else Div(
+                  Span(A2(f"Submission State: {submission_state} {prefix_type}{title}", href=url, target="_blank")), 
+                  Span(f"({parsed_url.netloc}, {created_at} by {owner} {state})", cls="text-xs text-gray-400"), 
                   cls="flex flex-col"
               )
     except ValueError:
@@ -53,6 +77,8 @@ def page_header(_title, auth, *args):
               Div(
                   A2('home', href='/'), 
                   '|',
+                  A2('submissions', href='/submissions'), 
+                  '|',
                   A2('submit', href='/submit'), 
                   '|',
                   A2(auth['username'], href='/profile') if auth else A2('login', href='/login') , 
@@ -60,5 +86,5 @@ def page_header(_title, auth, *args):
   return Title(title), Container(top, *args)
 
 
-__all__ = ['A2', 'display_time', 'display_url', 'page_header']
+__all__ = ['A2', 'display_time', 'display_url', 'display_submission_url', 'page_header']
 
